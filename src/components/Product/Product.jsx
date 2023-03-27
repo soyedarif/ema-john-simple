df84e91f75392509f7ffd7e5ef31ccee5eb0fb36
soyedarif <soyedariff@gmail.com>
2023-03-27 16:42:45 +0600

    product card done

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import './Product.css'
-const Product = (props) => {
-    console.log(props.product);
-    const {name,img,price,ratings,seller}=props.product;
-    return (
-        <div className='product'>
-            <img src={img} alt="" />
-        </div>
-    );
+import React from "react";
+import "./Product.css";
+const Product = props => {
+  const { name, img, price, ratings, seller } = props.product;
+  return (
+    <div className="product">
+      <img src={img} alt="" />
+      <div className="product-info">
+          <h6 className="product-name">{name}</h6>
+          <p>Price: ${price}</p>
+          <p>Manufacturer: {seller}</p>
+          <p>Rating: {ratings} Stars</p>
+      </div>
+      <button className="btn-cart">Add to Cart</button>
+    </div>
+  );
 };
 
 export default Product;
